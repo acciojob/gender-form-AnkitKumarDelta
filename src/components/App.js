64be270b69bb508cc96564ef075bb7c10c207804
AnkitKumarDelta <ankitna355@gmail.com>
2025-11-30 12:20:30 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -25,22 +25,27 @@ const App = () => {
         <input type="radio" name="gender" value="female" onChange={(e) => handleInput(e)} />
         Female
       </label>
-      <h2>Select your dress size:</h2>
       {
         input == "male" ? (
+          <>
+          <h2>Select your shirt size:</h2>
           <select value={size} onChange={(e)=>handleSize(e)}>
             <option>Select size</option>
             <option>Small</option>
             <option>Medium</option>
             <option>Large</option>
           </select>
+          </>
         ) : (
+          <>
+          <h2>Select your dress size:</h2>
           <select value={size} onChange={(e)=>handleSize(e)}>
             <option>Select size</option>
             <option>2</option>
             <option>4</option>
             <option>6</option>
           </select>
+          </>
         )
       }
     </div>
